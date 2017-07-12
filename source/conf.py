b599ab35481489ead344182a7b77032fdcc5feac
Domen Kožar <domen@dev.si>
2017-07-12 15:22:42 +0200

    Fix conf

--- source/conf.py
+++ source/conf.py
@@ -236,7 +236,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'nixpkgs-cookbook.tex', 'nixpkgs-cookbook Documentation',
-     'Domen Kožar', 'manual'),
+     u'Domen Kožar', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
