23479e0f0ec42d3c57b0cd7f717c116ab447d42a
Nejc Zupan <nejc.zupan@gmail.com>
2021-12-09 10:15:25 +0000

    Update Python env

--- flake.nix
+++ flake.nix
@@ -12,11 +12,24 @@
           inherit system;
           overlays = [ poetry2nix.overlay ];
         };
+        poetryOverrides = self: super: {
+
+          # Refs https://github.com/nix-community/poetry2nix/issues/218#issuecomment-981615612
+          typing-extensions = super.typing-extensions.overridePythonAttrs (
+            old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ self.flit-core ];
+            }
+          );
+        };
       in rec {
         packages = flake-utils.lib.flattenTree {
           nix-dev-pyenv = pkgs.poetry2nix.mkPoetryEnv {
             projectDir = self;
             python = pkgs.python38;
+            overrides = [
+              pkgs.poetry2nix.defaultPoetryOverrides
+              poetryOverrides
+            ];
           };
           nix-dev-html = pkgs.stdenv.mkDerivation {
             name = "nix-dev";
