d3cdd5c4aed0d9d75d76266c78ee1b85e91a2f62
Nejc Zupan <nejc.zupan@gmail.com>
2022-05-01 09:31:42 +0100

    Exit run_code_block_tests.sh with non-zero exit code if any test fails
    Turns out `find exec`/`execdir` will always exit with 0, so we have to
    use xargs instead.

--- run_code_block_tests.sh
+++ run_code_block_tests.sh
@@ -1,4 +1,4 @@
 set -euo pipefail
 
-find extracted -name "*.shell_session" -exec echo "running {}" \; -exec ./convert_shell_session_to_pexpect_test.py {} \;
-find extracted -name "test_*"  -exec echo "running {}" \; -execdir {} \;
+find extracted -name "*.shell_session" | xargs -I{} sh -c 'echo "running {}"; ./convert_shell_session_to_pexpect_test.py {};'
+find extracted -name "test_*" | xargs -I{} sh -c 'echo "running {}"; cd `dirname {}`; ./`basename {}`'
