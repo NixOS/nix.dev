7c52a2afd5a04550d3249200021973efb13a52fd
Nejc Zupan <nejc.zupan@gmail.com>
2022-05-02 23:03:03 +0100

    find/xargs improvements suggested by @cheriimoya

--- run_code_block_tests.sh
+++ run_code_block_tests.sh
@@ -1,4 +1,4 @@
 set -euo pipefail
 
-find extracted -name "*.shell_session" | xargs -I{} sh -c 'echo "running {}"; ./convert_shell_session_to_pexpect_test.py {};'
-find extracted -name "test_*" | xargs -I{} sh -c 'echo "running {}"; cd `dirname {}`; ./`basename {}`'
+find extracted -name "*.shell_session" -print0 | xargs -0 -I{} sh -c 'echo "running {}"; ./convert_shell_session_to_pexpect_test.py {};'
+find extracted -name "test_*" -print0 | xargs -0 -I{} sh -c 'echo "running {}"; cd `dirname {}`; ./`basename {}`'
