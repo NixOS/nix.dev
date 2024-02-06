3ffb8d822789319c9c67b59c4530ed371f8b4c3e
Valentin Gagarin <valentin.gagarin@tweag.io>
2024-02-07 00:25:38 +0100

    rework the way redirects are built

--- default.nix
+++ default.nix
@@ -3,26 +3,30 @@
 ,
 }:
 let
-  pkgs = import inputs.nixpkgs {
+  pkgs = import inputs.nixpkgs-prev-stable {
     config = { };
     overlays = [ (import ./overlay.nix) ];
     inherit system;
   };
+  lib = pkgs.lib;
 
   nix-dev =
     let
-      # Various versions of the Nix manuals, grep for (nix-manual)= to find where they are displayed
+      # Various versions of the Nix manuals, grep for (nix-manual)= to find where they are displayed.
       # FIXME: This requires human interaction to update! See ./CONTRIBUTING.md for details.
-      nix-releases = {
-        latest = "2.19";
-        rolling = "2.18";
-        stable = "2.18";
-        prev-stable = "2.13";
+      releases = rec {
+        nixpkgs-rolling = import inputs.nixpkgs-rolling { } // { inherit (nixpkgs-rolling.lib) version; };
+        nixpkgs-stable = import inputs.nixpkgs-stable { } // { inherit (nixpkgs-stable.lib) version; };
+        nixpkgs-prev-stable = import inputs.nixpkgs-prev-stable { } // { inherit (nixpkgs-prev-stable.lib) version; };
+        nix-latest = (import inputs.nix-latest).default;
+        # TODO: to further simplify this and get Nix from Nixpkgs with all required files present,
+        # make a patch release of Nix after https://github.com/NixOS/nix/pull/9949 lands,
+        # and bump the respective version in the respective Nixpkgs `release-*` branch.
+        nix-rolling = (import inputs.nix-rolling).default;
+        nix-stable = (import inputs.nix-stable).default;
+        nix-prev-stable = (import inputs.nix-prev-stable).default;
       };
-      nixpkgs-releases = {
-        stable = "23.11";
-        prev-stable = "23.05";
-      };
+      version = package: lib.versions.majorMinor package.version;
     in
     pkgs.stdenv.mkDerivation {
       name = "nix-dev";
@@ -40,47 +44,57 @@ let
       buildPhase =
         let
           nix-manual-index =
-            with pkgs.lib.attrsets;
-            let
-              raw = builtins.readFile ./source/reference/nix-manual.md;
-              nix-replacements = mapAttrsToList (release: version: "@${release}@") nix-releases;
-              nixpkgs-replacements = mapAttrsToList (release: version: "@nixpkgs-${release}@") nixpkgs-releases;
-            in
-            pkgs.lib.strings.replaceStrings
-              (nix-replacements ++ nixpkgs-replacements)
-              (attrValues nix-releases ++ attrValues nixpkgs-releases)
-              raw;
+            with lib.attrsets;
+            with lib.strings;
+            replaceStrings
+              (mapAttrsToList (release: _: "@${release}@") releases)
+              (mapAttrsToList (_: package: version package) releases)
+              (builtins.readFile ./source/reference/nix-manual.md);
         in
         ''
           cp -f ${builtins.toFile "nix-manual.md" nix-manual-index} $TMP/nix.dev/source/reference/nix-manual.md
           make html
         '';
       installPhase =
-        with pkgs.lib.attrsets;
-        with pkgs.lib.strings;
+        with lib.attrsets;
+        with lib.strings;
         let
-          nix-versions = with pkgs.lib; lists.unique (attrsets.attrValues nix-releases);
-          inputName = version: pkgs.lib.strings.replaceStrings [ "." ] [ "-" ] version;
-          nix-src = version: inputs."nix_${inputName version}";
-          nix-manual = version: (import (nix-src version)).default.doc;
-          copy = version: ''
-            cp -Rf ${nix-manual version}/share/doc/nix/manual/* $out/manual/nix/${version}
+          nix-releases =
+            let
+              package = name: elemAt (splitString "-" name) 0;
+              release = name: elemAt (splitString "-" name) 1;
+              filtered = filterAttrs (name: value: (package name) == "nix") releases;
+            in
+            mapAttrs' (name: value: { name = release name; inherit value; }) filtered;
+          # the same Nix version could appear in multiple Nixpkgs releases,
+          # but we want to copy each exactly once.
+          unique-version =
+            let
+              version-exists = p: ps: elem (version p) (map (x: version x) ps);
+            in
+            lib.lists.foldl' (acc: elem: if version-exists elem acc then acc else acc ++ [ elem ]) [ ];
+          copy = nix: ''
+            cp -Rf ${nix.doc}/share/doc/nix/manual/* $out/manual/nix/${version nix}
           '';
-          # add upstream page redirects of the form `<from> <to> <status>`, excluding comment lines and empty
-          redirects = version: ''
-            sed '/^#/d;/^$/d;s#^\(.*\) \(.*\) #/manual/nix/${version}\1 /manual/nix/${version}\2 #g' ${nix-src version}/doc/manual/_redirects >> $out/_redirects
+          # add upstream page redirects of the form `<from> <to> <status>`, excluding comments and empty lines
+          # TODO: once https://github.com/NixOS/nix/pull/9949 lands, bump the source and use:
+          #       ${nix.doc}/share/doc/nix/manual/_redirects
+          # also remove the then unnecessary file from the root directory of the manual:
+          #        rm $out/manual/nix/${version nix}/_redirects
+          redirects = nix: ''
+            sed '/^#/d;/^$/d;s#^\(.*\) \(.*\) #/manual/nix/${version nix}\1 /manual/nix/${version nix}\2 #g' ${nix.src}/doc/manual/_redirects >> $out/_redirects
           '';
-          shortlink = release: version: ''
-            echo /manual/nix/${release} /manual/nix/${version}/ 302 >> $out/_redirects
+          shortlink = release: nix: ''
+            echo /nix/manual/${release} /nix/manual/${nix.version}/ 302 >> $out/_redirects
           '';
         in
         ''
           mkdir -p $out
           cp -R build/html/* $out/
           # NOTE: the comma in the shell expansion makes it also work for singleton lists
-          mkdir -p $out/manual/nix/{${concatStringsSep "," nix-versions},}
-          ${concatStringsSep "\n" (map copy nix-versions)}
-          ${concatStringsSep "\n" (map redirects nix-versions)}
+          mkdir -p $out/manual/nix/{${concatStringsSep "," (mapAttrsToList (_: nix: version nix) nix-releases)},}
+          ${concatStringsSep "\n" (map copy (unique-version (attrValues nix-releases)))}
+          ${concatStringsSep "\n" (map redirects (unique-version (attrValues nix-releases)))}
           ${concatStringsSep "\n" (mapAttrsToList shortlink nix-releases)}
         '';
     };
