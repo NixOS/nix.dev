0b3e512d5e0a8e379eba729d56aaafacc5a0dc68
Pete Dietl <pdietl@anduril.com>
2024-05-14 22:43:26 +0200

    module system deep dive: minor improvements
    Add more error checking to the `map.sh` and `geocode.sh` script
    
    The default zoom of 2 results in the `center` option not showing anything
    different. So, we change the default zoom to 10.
    
    We also clear up the fact that using an option type of `nullOr <blah>` does not
    set a default value of `null` for you.

--- source/tutorials/module-system/files/map.sh
+++ source/tutorials/module-system/files/map.sh
@@ -1,60 +1,47 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cachedir=${XDG_CACHE_HOME:-~/.cache}/google-api/maps-static
-mkdir -p "$cachedir"
-hash=$(echo "$@" | sha256sum - | cut -d' ' -f1)
-cachefile="$cachedir/$hash"
+keyFile=${XDG_DATA_HOME:-~/.local/share}/google-api/key
 
-if [[ ! -f "$cachefile" ]]; then
-
-  keyFile=${XDG_DATA_HOME:-~/.local/share}/google-api/key
-
-  if [[ ! -f "$keyFile" ]]; then
+if [[ ! -f "$keyFile" ]]; then
     mkdir -p "$(basename "$keyFile")"
     echo "No Google API key found in $keyFile" >&2
     echo "For getting one, see https://developers.google.com/maps/documentation/maps-static/start#before-you-begin" >&2
     exit 1
-  fi
+fi
 
-  key=$(cat "$keyFile")
+key=$(cat "$keyFile")
+tmp=$(mktemp -d)
+trap 'rm -rf "$tmp"' exit
 
+output=$tmp/output
 
-  tmp=$(mktemp -d)
-  trap 'rm -rf "$tmp"' exit
-
-  output=$tmp/output
-
-  curlArgs=(
+curlArgs=(
     https://maps.googleapis.com/maps/api/staticmap
     --silent --show-error --get --output "$output" --write-out %{http_code}
-  )
+)
 
-  for arg in "$@"; do
+for arg in "$@"; do
     curlArgs+=(--data-urlencode "$arg")
-  done
+done
 
-  #echo curl ''${curlArgs[@]@Q} >&2
+echo curl ''${curlArgs[@]@Q} >&2
 
-  curlArgs+=(--data-urlencode key="$key")
+curlArgs+=(--data-urlencode key="$key")
 
-  if status=$(curl "${curlArgs[@]}"); then
-    if [[ "$status" == 200 ]]; then
-      mv "$output" "$cachefile"
-    else
-      echo "API returned non-200 HTTP status code $status, output is" >&2
-      cat "$output" >&2
-      exit 1
+if status=$(curl "${curlArgs[@]}"); then
+    if [[ "$status" != 200 ]]; then
+        echo "API returned non-200 HTTP status code $status, output is" >&2
+        cat "$output" >&2
+        exit 1
     fi
-  else
-    code=$?
-    echo "curl exited with code $code" >&2
+else
+    echo "curl exited with code $?" >&2
     exit 1
-  fi
 fi
 
 if [[ -t 1 ]]; then
-  echo "Successful, but won't output image to tty, pipe to a file or icat instead" >&2
+    echo "Successful, but won't output image to tty, pipe to a file or icat instead" >&2
 else
-  cat "$cachefile"
+    cat "$output"
 fi
